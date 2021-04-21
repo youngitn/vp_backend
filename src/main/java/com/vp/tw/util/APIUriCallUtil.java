17263704a4b7457b0a31d7d67e16357232a974fb
Yang Ting Cheng <youngtin1017@gmail.com>
2021-04-21 14:03:54 +0800

    try

---
+++ src/main/java/com/vp/tw/util/APIUriCallUtil.java
@@ -0,0 +1,54 @@
+package com.vp.tw.util;
+
+import java.nio.charset.Charset;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Data
+public class APIUriCallUtil {
+
+	
+
+	private static final CloseableHttpClient httpclient = HttpClients.createDefault();
+	// 宣告rep
+	private CloseableHttpResponse response = null;
+	
+	/**
+	 * 
+	* @Title: sendReq 
+	* @Description: 以POST方式送出req
+	* @param @param uri 資源位置
+	* @param @param contentType 
+	* @param @param charset
+	* @param @param reqJSONString    設定檔案 
+	* @return void    返回型別 
+	* @throws
+	 */
+	public void sendReq(String uri, String contentType, String charset, String reqJSONString) {
+		log.info("send req to " + uri);
+		HttpPost httppost = new HttpPost(uri);
+		httppost.addHeader("Content-Type", contentType);
+
+		StringEntity se = new StringEntity(reqJSONString, Charset.forName(charset));
+
+		httppost.setEntity(se);
+
+		String urlist = null;
+
+		try {
+			// send req
+			response = httpclient.execute(httppost);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
